19799500b976505cbf4e4204a1ec0c00bccdccfe
TMarquet <72377479+TMarquet@users.noreply.github.com>
2020-10-29 12:38:57 +0100

    changes

--- multi-gpu.py
+++ multi-gpu.py
@@ -1,43 +1,69 @@
 
+import tensorflow_datasets as tfds
 import tensorflow as tf
-from tensorflow import keras
-
-# Helper libraries
-import numpy as np
-import matplotlib.pyplot as plt
-print(tf.__version__)
 
 import os
 
-DEVICES = ['/device:GPU:0','/device:GPU:1']
-strategy = tf.distribute.MirroredStrategy(devices= DEVICES)
+datasets, info = tfds.load(name='mnist', with_info=True, as_supervised=True)
 
-fashion_mnist = keras.datasets.fashion_mnist
+mnist_train, mnist_test = datasets['train'], datasets['test']
 
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+strategy = tf.distribute.MirroredStrategy()
 
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-train_images = train_images / 255.0
 
-test_images = test_images / 255.0
+# DEVICES = ['/device:GPU:0','/device:GPU:1']
+# strategy = tf.distribute.MirroredStrategy(devices= DEVICES)
+
+num_train_examples = info.splits['train'].num_examples
+num_test_examples = info.splits['test'].num_examples
+
+BUFFER_SIZE = 10000
+
+BATCH_SIZE_PER_REPLICA = 64
+BATCH_SIZE = BATCH_SIZE_PER_REPLICA * strategy.num_replicas_in_sync
+
+def scale(image, label):
+  image = tf.cast(image, tf.float32)
+  image /= 255
+
+  return image, label
+
+train_dataset = mnist_train.map(scale).cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+eval_dataset = mnist_test.map(scale).batch(BATCH_SIZE)
+
 
 with strategy.scope():
-  input_re = keras.layers.Flatten(input_shape=(28, 28))
-  hid = keras.layers.Dense(128, activation='relu')
-  out =  keras.layers.Dense(10)
-  model = keras.Sequential([
-  input_re,
-  hid,
-  out
+  model = tf.keras.Sequential([
+      tf.keras.layers.Conv2D(32, 3, activation='relu', input_shape=(28, 28, 1)),
+      tf.keras.layers.MaxPooling2D(),
+      tf.keras.layers.Flatten(),
+      tf.keras.layers.Dense(64, activation='relu'),
+      tf.keras.layers.Dense(10)
   ])
 
-  model.compile(loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
+  model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
                 optimizer=tf.keras.optimizers.Adam(),
                 metrics=['accuracy'])
 
-# Define the checkpoint directory to store the checkpoints
+checkpoint_dir = './training_checkpoints'
+# Name of the checkpoint files
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
+# Function for decaying the learning rate.
+# You can define any decay function you need.
+def decay(epoch):
+  if epoch < 3:
+    return 1e-3
+  elif epoch >= 3 and epoch < 7:
+    return 1e-4
+  else:
+    return 1e-5
+callbacks = [
+    tf.keras.callbacks.TensorBoard(log_dir='./logs'),
+    tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_prefix,
+                                       save_weights_only=True),
+    tf.keras.callbacks.LearningRateScheduler(decay),
+]
+
+model.fit(train_dataset, epochs=12, callbacks=callbacks)
 
 
-
-model.fit(train_images, train_labels, epochs=10)
